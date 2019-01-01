3370e61bf8b821c4fe70f1d9be5114bb4d9e6bd5
paul <paul@wingmen.io>
2019-01-01 22:43:54 +0700

    docker SDK connection test

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace wingmen.io
+{
+    using System;
+    using Docker.DotNet;
+
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            AnonymousCredentials creds = new AnonymousCredentials();
+
+            DockerClient client = new DockerClientConfiguration(new Uri("http://127.0.0.1:3128")).CreateClient();
+
+            var containers = client.Containers.ListContainersAsync
+            (
+                new Docker.DotNet.Models.ContainersListParameters() 
+                { 
+                   All = true
+                }
+            ).Result;
+
+            foreach (var item in containers)
+            {
+                Console.WriteLine($"Id: {item.ID}");
+                Console.WriteLine($"Status: {item.Status}");
+            }
+        }
+    }
+}
